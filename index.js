9c3d8bf301c856f88e0f9f901542332bd614c1f4
Dean Stigall <stigallrealestate@gmail.com>
2017-12-01 01:26:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers, callback){
+const newArray = [];
+  for (const element of drivers){
+    newArray.push(callback(element.toLowerCase()));
+  }
+}
