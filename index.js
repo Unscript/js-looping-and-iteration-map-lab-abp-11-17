41f3cc3651be8b7491e124a0eb2284c36a995a55
Dean Stigall <stigallrealestate@gmail.com>
2017-12-01 01:34:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 // Code your solution in this file.
-function lowerCaseDrivers(drivers, callback){
-const newArray = [];
-  for (const element of drivers){
-    newArray.push(callback(element));
-  }
+function lowerCaseDrivers(drivers){
+const newDrivers = map(drivers, function(name))
 }
